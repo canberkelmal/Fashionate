b3de6e10daf5f9db65c943d8bd6300a323d16a97
Canberk Elmal <elmalcanberk@gmail.com>
2022-11-08 12:41:36 +0300

    EditSceneAndWalkerPoints

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    Vector3 WalkerStartPoint=new Vector3(16, 2, -21);
+    Vector3 WalkerEndPoint=new Vector3(15, 2, -14);
     
     void Start()
     {
