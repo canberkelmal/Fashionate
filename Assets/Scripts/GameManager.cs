58b73425961e5fc5c28205009e48674b252cae0d
Canberk Elmal <elmalcanberk@gmail.com>
2022-11-08 14:44:01 +0300

    CrossOver

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    Vector3 WalkerStartPoint=new Vector3(16, 2, -21);
+    Vector3 WalkerStartPoint=new Vector3(18, 2, -21);
     Vector3 WalkerEndPoint=new Vector3(15, 2, -14);
+    public GameObject CurrenWalker;
+    public float WalkerSpeed;
+    bool isArrived=false;
     
     void Start()
     {
         
     }
     
-    void Update()
+    void FixedUpdate()
     {
+        if(Input.GetKeyDown(KeyCode.R)){
+            Replay();
+        }
+
+        if(Input.GetMouseButton(0) && !isArrived){
+            Debug.Log("clicked");
+            CurrenWalker.transform.position=Vector3.MoveTowards(CurrenWalker.transform.position, WalkerEndPoint, WalkerSpeed);
+        }
+            
+        if(CurrenWalker.transform.localPosition.z+0.2 >= WalkerEndPoint.z && !isArrived){
+            WalkerArrived();
+        }
+
         
+        
+    }
+
+    void WalkerArrived(){
+        isArrived=true;
+        Debug.Log("Walker has arrived!");
+    }
+    public void Replay(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
