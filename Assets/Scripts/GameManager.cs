c6fc82c365dad7d30975f5c4e20110ec943bcca5
Canberk Elmal <elmalcanberk@gmail.com>
2022-11-08 15:43:49 +0300

    BetterCodesAndCrossOverDone

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,41 +24,53 @@ public class GameManager : MonoBehaviour
     
     void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.R)){
-            Replay();
-        }
-
+        //Set CurrentWalker position to the WalkerEndPoint via LeftClick or Touch
         if(Input.GetMouseButton(0)){
             Debug.Log("clicked");
             CurrenWalker.transform.position=Vector3.MoveTowards(CurrenWalker.transform.position, WalkerEndPoint, WalkerSpeed);
         }
-            
+
+        //WalkerArrived function is triggered when CurrentWalker is ALMOST arrive to the WalkerEndPoint
         if(CurrenWalker.transform.localPosition.z+0.2 >= WalkerEndPoint.z){
             WalkerArrived();
         }
 
-        if(CurrenWalker.transform.position.z>WalkerStartPoint.z+2 && !WalkStarted){
-            WalkStarted=true;
-        }
-
+        //WalkStarted is true if CurrentWalker is on the band
+        WalkStarted = (CurrenWalker.transform.position.z>WalkerStartPoint.z+2 && !WalkStarted) ? true : false;
         if(WalkStarted){
             SetWaitingWalkers();
         }
+
+        //Press "R" for reload the current scene
+        if(Input.GetKeyDown(KeyCode.R)){
+            Replay();
+        }
+
     }
 
+    //Set the pending walkers's positions
     void SetWaitingWalkers()
     {
         Walkers.transform.GetChild(1).transform.position=Vector3.MoveTowards(Walkers.transform.GetChild(1).transform.position, WalkerStartPoint, WalkerSpeed);
         Walkers.transform.GetChild(2).transform.position=Vector3.MoveTowards(Walkers.transform.GetChild(2).transform.position, PendingWalkerPos, WalkerSpeed);;
     }
 
+
     void WalkerArrived(){
+
+        //Creates 2nd pending walker
         Instantiate(CurrenWalker, PendingWalker2Pos, CurrenWalker.transform.rotation, Walkers.transform);
+
+        //Set CurrentWalker to next walker
         CurrenWalker=Walkers.transform.GetChild(1).gameObject;
+        WalkStarted=false;
+
+        //Destroy the arrived walker
         Destroy(Walkers.transform.GetChild(0).gameObject);
         Debug.Log("Walker has arrived!");
     }
 
+    //Reload the current scene
     public void Replay(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
