c9f28da721532ced6ea6a22066ec73b561907d6f
Canberk Elmal <elmalcanberk@gmail.com>
2022-11-08 14:59:57 +0300

    PendingWalkersPositions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,13 +7,17 @@ public class GameManager : MonoBehaviour
 {
     Vector3 WalkerStartPoint=new Vector3(18, 2, -21);
     Vector3 WalkerEndPoint=new Vector3(15, 2, -14);
+    Vector3 PendingWalkerPos;
+    Vector3 PendingWalker2Pos;
+    public GameObject Walkers;
     public GameObject CurrenWalker;
     public float WalkerSpeed;
     bool isArrived=false;
     
     void Start()
     {
-        
+        PendingWalkerPos = new Vector3(WalkerStartPoint.x+2, 2, WalkerStartPoint.z);
+        PendingWalker2Pos=new Vector3(PendingWalkerPos.x+2, 2, WalkerStartPoint.z);
     }
     
     void FixedUpdate()
